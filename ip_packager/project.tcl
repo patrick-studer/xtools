c121917894fca3b753e39bd374e9e94d0eaf4f38
Patrick Studer <studer.patrick92@gmail.com>
2024-05-28 14:17:35 +0200

    Reorganize vivado msg-configs

--- ip_packager/project.tcl
+++ ip_packager/project.tcl
@@ -51,15 +51,13 @@ proc ::xtools::ip_packager::_overwrite_msg_config {} {
 
     if {$config::MsgConfigOverwrite} {
         reset_msg_config -id  *                     -default_severity -quiet
+        set_msg_config   -id  {[Ipptcl 7-1550]}     -new_severity "INFO"
         set_msg_config   -id  {[Vivado 12-180]}     -new_severity "ERROR"
         set_msg_config   -id  {[Vivado 12-508]}     -new_severity "ERROR"
         set_msg_config   -id  {[Vivado 12-3631]}    -new_severity "INFO"
         set_msg_config   -id  {[Vivado 12-1348]}    -new_severity "INFO"
-        set_msg_config   -id  {Vivado 12-7122}      -suppress
-        set_msg_config   -id  {[Ipptcl 7-1550]}     -new_severity "INFO"
+        set_msg_config   -id  {[Vivado 12-7122]}    -new_severity "INFO"
         set_msg_config   -id  {[IP_Flow 19-234]}    -suppress
-        set_msg_config   -id  {Synth 8-7080}        -suppress
-        set_msg_config   -id  {Route 35-198}        -new_severity "INFO"
         set_msg_config   -id  {[IP_Flow 19-377]}    -new_severity "INFO"
         set_msg_config   -id  {[IP_Flow 19-459]}    -new_severity "INFO"
         set_msg_config   -id  {[IP_Flow 19-1700]}   -suppress
@@ -69,6 +67,7 @@ proc ::xtools::ip_packager::_overwrite_msg_config {} {
         set_msg_config   -id  {[IP_Flow 19-5226]}   -suppress
         set_msg_config   -id  {[IP_Flow 19-5905]}   -new_severity "INFO"
         set_msg_config   -id  {[filemgmt 20-730]}   -new_severity "INFO"
+        set_msg_config   -id  {[Route 35-198]}      -suppress
     } else {
         puts "INFO: \[_overwrite_msg_config\] Message Config Overwrite is disabled. Continue with the default Vivado settings."
     }
